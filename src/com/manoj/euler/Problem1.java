3bb749c0eb3637d35c312096a1cf629f2334421a
Manoj Kumar Chevvuri <ManojKumar.Chevvuri@davita.com>
2020-01-29 19:04:22 -0600

    init

--- src/com/manoj/euler/Problem1.java
+++ src/com/manoj/euler/Problem1.java
@@ -1,7 +1,5 @@
 package com.manoj.euler;
 
-import java.util.Scanner;
-
 public class Problem1 {
     /*
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
