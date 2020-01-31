52d0a6847a15d80ec203c68cc6c54ff5a8272d65
Manoj Kumar Chevvuri <ManojKumar.Chevvuri@davita.com>
2020-01-31 14:13:10 -0600

    problem 3 solution

--- src/com/manoj/euler/Problem3.java
+++ src/com/manoj/euler/Problem3.java
@@ -1,5 +1,10 @@
 package com.manoj.euler;
 
+import com.manoj.euler.utils.PrimeFactorUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 The prime factors of 13195 are 5, 7, 13 and 29.
 
@@ -7,6 +12,12 @@ What is the largest prime factor of the number 600851475143 ?
  */
 public class Problem3 {
     public static void main(String[] args) {
+
+        List<Long> result = PrimeFactorUtils.primeFactorsForInteger(600851475143l);
+
+        System.out.println("prime factors for: "+600851475143l+ " are " + result);
+
+        System.out.println("the largest factorial is: " + result.get(result.size()-1));
 
     }
 }
--- src/com/manoj/euler/utils/PrimeFactorUtils.java
+++ src/com/manoj/euler/utils/PrimeFactorUtils.java
@@ -1,8 +1,46 @@
 package com.manoj.euler.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class PrimeFactorUtils {
 
-    static int[] primeFactorsForInteger(int number){
-        return new int[0];
+    /**
+     *
+     * @param number input integer
+     * @return array of integers with all the prime factors in ascending order
+     */
+    public static List<Long> primeFactorsForInteger(long number){
+        List<Long> result = new ArrayList<>();
+        if(number <= 0){
+            System.out.println("number is either less than or equal to 0 : " + number);
+            result.add(0l);
+            return result;
+        } else if (1 == number){
+            System.out.println("number is equal to 1");
+            result.add(1l);
+            return result;
+        }else {
+            // loop to count how many times is given number is divisible by 2
+            while(number % 2 == 0){
+                result.add(2l);
+                number /= 2;
+            }
+
+            //once number is out of the above loop, it's odd number by then. so we'll try to find all the odd numbers until sqrt of number
+            for (long j = 3 ; j<= Math.sqrt(number); j += 2){
+                while(number % j == 0){
+                    result.add(j);
+                    number /= j;
+                }
+            }
+
+            // if left over number is more than 2 and out of both the above loops, that means the number is prime
+            if(number > 2){
+                result.add(number);
+            }
+        }
+
+        return result;
     }
 }
